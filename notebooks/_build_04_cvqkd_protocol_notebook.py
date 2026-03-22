55058196f1d9077c1385b71c2f191517c2f74e8d
Syed Abbas Ahmad <abbas.ahmad@cowlar.com>
2026-03-22 15:42:08 +0500

    Add build 04 cvqkd protocol notebook module scaffold

---
+++ notebooks/_build_04_cvqkd_protocol_notebook.py
@@ -0,0 +1,17 @@
+"""
+Builder for notebooks/04_cvqkd_protocol.ipynb (Notebook 04/05: GG02 protocol).
+
+Run:
+    python notebooks/_build_04_cvqkd_protocol_notebook.py
+
+Then execute:
+    python notebooks/_execute_notebook.py 04_cvqkd_protocol.ipynb
+"""
+
+from pathlib import Path
+
+import nbformat as nbf
+
+NB_PATH = Path(__file__).resolve().parent / "04_cvqkd_protocol.ipynb"
+
+
